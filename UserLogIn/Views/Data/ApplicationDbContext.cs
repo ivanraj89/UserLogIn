a9db3c0d77eeaf036571db8ed86e7c1042806523
ivanraj89 <ivanraj89@gmail.com>
2023-08-30 05:40:03 +0800

    Commit

---
+++ UserLogIn/Views/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UserLogIn.Views.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+    }
+}
